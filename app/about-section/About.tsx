b4d101274743b1ccd387314acd5e8a7f0285ab0c
vikramchavan888 <vikramchavan69ps@gmail.com>
2025-01-31 21:07:13 +0530

    modified

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -108,7 +108,7 @@ const About = () => {
         <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
           <SongCarousel />
           <AnimatedBody
-            text="A few songs I can recommend if you're looking for some fresh tunes :)"
+            text="Above are some certification done be me :)"
             className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] "
           />
         </div>
