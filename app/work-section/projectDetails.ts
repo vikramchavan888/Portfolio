198ed5108fbcdf05449d4a03af96ac5587a9b2f3
vikramchavan888 <vikramchavan69ps@gmail.com>
2025-02-06 22:23:14 +0530

    iibn

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -12,28 +12,6 @@ export type ProjectProps = {
 export const devProjects = [
   {
     id: 0,
-    name: "Form-Builder",
-    description:
-      "form builder is a app where one set of users can create forms and view form responses. And on the other hand, other set of users can fill the forms which are shared through public links.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/vikramchavan888/-form-builder",
-    demo: "https://form-builder-app-delta.vercel.app/",
-    image: require(".//../../public/projects/Screenshot (211).png"),
-    available: true,
-  },
-  {
-    id: 1,
-    name: "Interlock",
-    description:
-      "This is a website for a Fintech Startup to showcase their innovative solutions tailored to meet the evolving needs of their clients.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/vikramchavan888/interlock",
-    demo: "https://interlock-eight.vercel.app/",
-    image: require(".//../../public/projects/interlock.png"),
-    available: true,
-  },
-  {
-    id: 2,
     name: "Synthetix",
     description:
       "Built specifically for an AI startup, this website lets them present cutting-edge AI data processing solutions tailored to their customers' needs.",
@@ -41,6 +19,28 @@ export const devProjects = [
     github: "https://github.com/vikramchavan888/synthetix",
     demo: "https://synthetix-6979.vercel.app/",
     image: require(".//../../public/projects/synthetix-flip.png"),
+    available: true,
+  },
+  {
+    id: 1,
+    name: "Form-Builder",
+    description:
+      "form builder is a app where one set of users can create forms and view form responses. And on the other hand, other set of users can fill the forms which are shared through public links.",
+    technologies: ["React", "CSS", "Mongo DB"],
+    github: "https://github.com/vikramchavan888/-form-builder",
+    demo: "https://form-builder-app-delta.vercel.app/",
+    image: require(".//../../public/projects/Screenshot (211).png"),
+    available: true,
+  },
+  {
+    id: 2,
+    name: "Interlock",
+    description:
+      "This is a website for a Fintech Startup to showcase their innovative solutions tailored to meet the evolving needs of their clients.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion"],
+    github: "https://github.com/vikramchavan888/interlock",
+    demo: "https://interlock-eight.vercel.app/",
+    image: require(".//../../public/projects/interlock.png"),
     available: true,
   },
   {
