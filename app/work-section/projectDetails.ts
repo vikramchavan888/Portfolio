41a1d0a0199211c87ebef56e6d378656052fc32c
vikramchavan888 <vikramchavan69ps@gmail.com>
2025-02-06 22:05:09 +0530

    hgkg

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -12,13 +12,13 @@ export type ProjectProps = {
 export const devProjects = [
   {
     id: 0,
-    name: "Odunsi",
+    name: "Form-Builder",
     description:
-      "Portfolio website for Michael Odunsi, an experienced UI/UX designer crafting unique, user-friendly products and web experiences for Web3 founders and projects.",
+      "form builder is a app where one set of users can create forms and view form responses. And on the other hand, other set of users can fill the forms which are shared through public links.",
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "",
-    demo: "https://www.odunsi.xyz/",
-    image: require(".//../../public/projects/odunsi.png"),
+    github: "https://github.com/vikramchavan888/-form-builder",
+    demo: "https://form-builder-app-delta.vercel.app/",
+    image: require(".//../../public/projects/Screenshot (211).png"),
     available: true,
   },
   {
@@ -27,8 +27,8 @@ export const devProjects = [
     description:
       "This is a website for a Fintech Startup to showcase their innovative solutions tailored to meet the evolving needs of their clients.",
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "",
-    demo: "https://interlock-teal.vercel.app/",
+    github: "https://github.com/vikramchavan888/interlock",
+    demo: "https://interlock-eight.vercel.app/",
     image: require(".//../../public/projects/interlock.png"),
     available: true,
   },
@@ -38,8 +38,8 @@ export const devProjects = [
     description:
       "Built specifically for an AI startup, this website lets them present cutting-edge AI data processing solutions tailored to their customers' needs.",
     technologies: ["React", "Next.js", "Prismic CMS"],
-    github: "",
-    demo: "https://synthetix-iota.vercel.app/",
+    github: "https://github.com/vikramchavan888/synthetix",
+    demo: "https://synthetix-6979.vercel.app/",
     image: require(".//../../public/projects/synthetix-flip.png"),
     available: true,
   },
@@ -49,124 +49,9 @@ export const devProjects = [
     description:
       "A website built for a software startup and small business, to showcase their services and mark their online presence.",
     technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "",
-    demo: "https://propellent.vercel.app/",
+    github: "https://github.com/vikramchavan888/propellent",
+    demo: "https://propellent-beta.vercel.app/",
     image: require(".//../../public/projects/propellent-new.png"),
     available: true,
-  },
-  {
-    id: 4,
-    name: "Flixify",
-    description:
-      "Flixify lets you seamlessly explore movies and TV series, add bookmarks, and search across all pages. It offers user authentication along with a theme switch.",
-    technologies: ["Next.js", "Typescript", "Prisma"],
-    github: "",
-    demo: "https://flixify.victorwilliams.me/",
-    image: require(".//../../public/projects/flixify.png"),
-    available: true,
-  },
-  // {
-  //   id: 4,
-  //   name: "SkyWatch",
-  //   description:
-  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-  //   technologies: ["React", "CSS", "Chart.js"],
-  //   github: "https://github.com/victorcodess/weather-forecast-website",
-  //   demo: "https://sky-watch.vercel.app/",
-  //   image: require(".//../../public/projects/skywatch-flip.png"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Alpaca Image Generator",
-  //   description:
-  //     "An image generator website that allows users to generate, combine, and download images.",
-  //   technologies: ["React", "CSS", "Merge Images"],
-  //   github: "https://github.com/victorcodess/alpaca-image-generator",
-  //   demo: "http://alpaca-image-generator-beta.vercel.app",
-  //   image: require(".//../../public/projects/alpaca-flip.png"),
-  //   available: true,
-  // },
-
-  // {
-  //   id: 5,
-  //   name: "Link Shortener",
-  //   description:
-  //     "A website that reduces the length of your URL using Bit.ly's API",
-  //   technologies: ["JavaScript", "CSS", "Bit.ly's API"],
-  //   github: "https://github.com/victorcodess/url-shortener",
-  //   demo: "https://url-shortener-nine-delta.vercel.app",
-  //   image: require(".//../../public/projects/shortener-new.webp"),
-  //   available: true,
-  // },
-  // {
-  //   id: 6,
-  //   name: "Carpooling Service",
-  //   description:
-  //     "TMTM helps Covenant University students find fellow students who are headed to the same location, so they can share a ride and split the cost.",
-  //   technologies: ["Material UI", "React", "Formik"],
-  //   github: "https://github.com/victorcodess/carpooling-service",
-  //   demo: "",
-  //   image: require(".//../../public/projects/carpool-new.webp"),
-  //   available: false,
-  // },
-  // {
-  //   id: 7,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-frontend",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
-  // },
-];
-
-export const designProjects = [
-  {
-    id: 1,
-    name: "Hebron Statup Lab Website",
-    description:
-      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 2,
-    name: "RAGS Scrubs Website",
-    description:
-      "An image generator website that allows users to generate, combine, and download images.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 3,
-    name: "Crown Branding Agency Website",
-    description:
-      "A website that reduces the length of your URL using Bit.ly's API",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image:
-      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 4,
-    name: "Titi Mobile App",
-    description:
-      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
-    available: false,
   },
 ];
